7e8586923c9d2e9447d7c67e68987004cbe9ce94
Ben Turney <btatvt11@gmail.com>
2014-10-17 12:36:10 -0400

    Testing out package structure.  Seems to work okay.

---
+++ src/week2/lab2/Lab2.java
@@ -0,0 +1,10 @@
+package week2.lab2;
+
+/**
+ * Created by BTurney on 10/17/2014.
+ */
+public class Lab2 {
+    public static void main(String[] args) {
+        System.out.println("Lab 2");
+    }
+}
---
+++ src/week2/problem9_1/Solution.java
@@ -0,0 +1,10 @@
+package week2.problem9_1;
+
+/**
+ * Created by BTurney on 10/17/2014.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("solution");
+    }
+}
