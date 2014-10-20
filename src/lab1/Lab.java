7b0e2b29aef37453bb38aa95ba00b2ea870a2730
Ben Turney <btatvt11@gmail.com>
2014-10-19 22:32:46 -0400

    Solution found. Just need to clean up.

--- src/lab1/Lab.java
+++ src/lab1/Lab.java
@@ -18,21 +18,41 @@ public class Lab {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        float[] vars = new float[10];
-        float sum = 0;
-        float mean;
+        double[] vars = new double[10];
+        double mean, variance, stddev;
 
         System.out.print("Enter ten numbers: ");
 
         //Read input
         for (int i = 0; i < vars.length; i++)
-            vars[i] = in.nextFloat();
+            vars[i] = in.nextDouble();
 
-        for (float var : vars)
-            sum += var;
+        // Calculate the mean
+        mean = findAverage(vars);
 
-        mean = sum / vars.length;
+        //Subtract the mean from each number and square the result (the squared difference)
+        for (int i = 0; i < vars.length; i++) {
+            vars[i] = Math.pow((vars[i] - mean), 2);
+            System.out.println(vars[i]);
+        }
+
+        //Calculate average of new results
+        variance = findAverage(vars);
+
+        // Standard deviation is square root of the variance
+        stddev = Math.sqrt(variance);
+
+        //Output results
         System.out.println("The mean is " + mean);
+        System.out.println("The standard deviation is " + stddev);
 
+    }
+
+    private static double findAverage(double[] dataset) {
+        double sum = 0;
+        for (double data : dataset) {
+            sum += data;
+        }
+        return sum / dataset.length;
     }
 }
