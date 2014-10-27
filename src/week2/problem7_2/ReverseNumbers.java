89995a2ba00f7c5b5095034897ef90393e2b5afb
Ben Turney <btatvt11@gmail.com>
2014-10-26 21:31:26 -0400

    Finish problem 7.2

---
+++ src/week2/problem7_2/ReverseNumbers.java
@@ -0,0 +1,25 @@
+package week2.problem7_2;
+
+import java.util.Scanner;
+
+/**
+ * Reads ten integers and displays them in the reverse of the order in which they were read.
+ *
+ * @author Ben Turney
+ */
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] numbers = new int[10];
+
+        System.out.print("Enter ten integers: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = in.nextInt();
+        }
+
+        System.out.println("Your numbers reversed: ");
+        for (int i = numbers.length; i > 0; i--) {
+            System.out.print(numbers[i - 1] + " ");
+        }
+    }
+}
